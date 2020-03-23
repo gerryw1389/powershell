6ad9a61d9dd093c504a31595050be16333eab987
Gerry Williams <gerryw1389@gmail.com>
2020-03-23 11:34:04 -0500

    update templates

--- Other/templates/_current-template-w-logging.ps1
+++ Other/templates/_current-template-w-logging.ps1
@@ -3,42 +3,70 @@
 # Name: Set-Template
 # Copyright: Gerry Williams (https://automationadmin.com)
 # License: MIT License (https://opensource.org/licenses/mit)
-# Script Modified from: n/a
 <#######</Header>#######>
 <#######<Body>#######>
 Function Set-Template
 {
-    <#
-    .Synopsis
-    Light description.
-    .Description
-    More thorough description.
-    .Example
-    Set-Template
-    Usually same as synopsis.
-    .Notes
-    Please see https://www.gerrywilliams.net/2017/09/running-ps-scripts-against-multiple-computers/ on how to run against multiple computers.
-    #>
+   <#
+.Synopsis
+Light description
+.Description
+More thorough description.
+.Example
+Set-Template
+Light description
+#>
 
-    [Cmdletbinding()]
+   [Cmdletbinding()]
+   Param
+   (
+   )
     
-    Param
-    (
-    )
-    
-    Begin
-    {       
+   Begin
+   {
+      ####################<Default Begin Block>####################
+      # Force verbose because Write-Output doesn't look well in transcript files
+      $VerbosePreference = "Continue"
         
-        ####################<Default Begin Block>####################
-        # Force verbose because Write-Output doesn't look well in transcript files
-        $VerbosePreference = "Continue"
+      [String]$Logfile = $PSScriptRoot + '\PSLogs\' + (Get-Date -Format "yyyy-MM-dd") +
+      "-" + $MyInvocation.MyCommand.Name + ".log"
+      
+      Function Start-Log
+      {
+         <#
+            .Synopsis
+            Creates the log file and starts transcribing the session.
+            .Notes
+            2018-06-24: Initial script
+            #>
+            
+         # Create transcript file if it doesn't exist
+         If (!(Test-Path $Logfile))
+         {
+            New-Item -Itemtype File -Path $Logfile -Force | Out-Null
+         }
         
-        [String]$Logfile = $PSScriptRoot + '\PSLogs\' + (Get-Date -Format "yyyy-MM-dd") +
-        "-" + $MyInvocation.MyCommand.Name + ".log"
-        
-        Function Write-Log
-        {
-            <#
+         # Clear it if it is over 10 MB
+         [Double]$Sizemax = 10485760
+         $Size = (Get-Childitem $Logfile | Measure-Object -Property Length -Sum) 
+         If ($($Size.Sum -ge $SizeMax))
+         {
+            Get-Childitem $Logfile | Clear-Content
+            Write-Verbose "Logfile has been cleared due to size"
+         }
+         Else
+         {
+            #Write-Verbose "Logfile was less than 10 MB"   
+         }
+         Start-Transcript -Path $Logfile -Append 
+         Write-Log "##########<Start Function>##########"
+         Write-Log "Function started on $env:COMPUTERNAME"
+
+      }
+
+      Function Write-Log
+      {
+         <#
             .Synopsis
             This writes objects to the logfile and to the screen with optional coloring.
             .Parameter InputObject
@@ -58,195 +86,109 @@ Function Set-Template
             2018-06-24: Initial script
             #>
             
-            Param
-            (
-                [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
-                [PSObject]$InputObject,
+         Param
+         (
+            [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+            [PSObject]$InputObject,
                 
-                # I usually set this to = "Green" since I use a black and green theme console
-                [Parameter(Mandatory = $False, Position = 1)]
-                [Validateset("Black", "Blue", "Cyan", "Darkblue", "Darkcyan", "Darkgray", "Darkgreen", "Darkmagenta", "Darkred", `
-                        "Darkyellow", "Gray", "Green", "Magenta", "Red", "White", "Yellow")]
-                [String]$Color = "Green"
-            )
+            [Parameter(Mandatory = $False, Position = 1)]
+            [Validateset("Black", "Blue", "Cyan", "Darkblue", "Darkcyan", "Darkgray", "Darkgreen", "Darkmagenta", "Darkred", `
+                  "Darkyellow", "Gray", "Green", "Magenta", "Red", "White", "Yellow")]
+            [String]$Color = ""
+         )
             
-            $ConvertToString = Out-String -InputObject $InputObject -Width 100
+         $ConvertToString = Out-String -InputObject $InputObject -Width 100
             
-            If ($($Color.Length -gt 0))
-            {
-                $previousForegroundColor = $Host.PrivateData.VerboseForegroundColor
-                $Host.PrivateData.VerboseForegroundColor = $Color
-                Write-Verbose -Message "$(Get-Date -Format "yyyy-MM-dd hh:mm:ss tt"): $ConvertToString"
-                $Host.PrivateData.VerboseForegroundColor = $previousForegroundColor
-            }
-            Else
-            {
-                Write-Verbose -Message "$(Get-Date -Format "yyyy-MM-dd hh:mm:ss tt"): $ConvertToString"
-            }
-            
-        }
+         If ($($Color.Length -gt 0))
+         {
+            $previousForegroundColor = $Host.PrivateData.VerboseForegroundColor
+            $Host.PrivateData.VerboseForegroundColor = $Color
+            Write-Verbose -Message "$(Get-Date -Format "yyyy-MM-dd hh:mm:ss tt"): $ConvertToString"
+            $Host.PrivateData.VerboseForegroundColor = $previousForegroundColor
+         }
+         Else
+         {
+            Write-Verbose -Message "$(Get-Date -Format "yyyy-MM-dd hh:mm:ss tt"): $ConvertToString"
+         }
+      }
 
-        Function Start-Log
-        {
-            <#
-            .Synopsis
-            Creates the log file and starts transcribing the session.
-            .Notes
-            2018-06-24: Initial script
-            #>
-            
-            # Create transcript file if it doesn't exist
-            If (!(Test-Path $Logfile))
-            {
-                New-Item -Itemtype File -Path $Logfile -Force | Out-Null
-            }
-        
-            # Clear it if it is over 10 MB
-            [Double]$Sizemax = 10485760
-            $Size = (Get-Childitem $Logfile | Measure-Object -Property Length -Sum) 
-            If ($($Size.Sum -ge $SizeMax))
-            {
-                Get-Childitem $Logfile | Clear-Content
-                Write-Verbose "Logfile has been cleared due to size"
-            }
-            Else
-            {
-                Write-Verbose "Logfile was less than 10 MB"   
-            }
-            Start-Transcript -Path $Logfile -Append 
-            Write-Log "####################<Function>####################"
-            Write-Log "Function started on $env:COMPUTERNAME"
-
-        }
-        
-        Function Stop-Log
-        {
-            <#
+      Function Stop-Log
+      {
+         <#
             .Synopsis
             Stops transcribing the session and cleans the transcript file by removing the fluff.
             .Notes
             2018-06-24: Initial script
             #>
             
-            Write-Log "Function completed on $env:COMPUTERNAME"
-            Write-Log "####################</Function>####################"
-            Stop-Transcript
+         Write-Log "Function completed on $env:COMPUTERNAME"
+         Write-Log "###########<End Function>###########"
+         Stop-Transcript
        
-            # Now we will clean up the transcript file as it contains filler info that needs to be removed...
-            $Transcript = Get-Content $Logfile -raw
+         # Now we will clean up the transcript file as it contains filler info that needs to be removed...
 
-            # Create a tempfile
-            $TempFile = $PSScriptRoot + "\PSLogs\temp.txt"
-            New-Item -Path $TempFile -ItemType File | Out-Null
-			
-            # Get all the matches for PS Headers and dump to a file
-            $Transcript | 
-                Select-String '(?smi)\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*([\S\s]*?)\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*' -AllMatches | 
-                ForEach-Object {$_.Matches} | 
-                ForEach-Object {$_.Value} | 
-                Out-File -FilePath $TempFile -Append
+         # Remove Start-Transcript auto generated stuff
+         $LogfileContent = get-content $Logfile -Raw
+         $splat = @{
+            'InputObject' = $LogfileContent
+            'Pattern'     = '(?smi)\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*([\S\s]*?)\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*'
+            'AllMatches'  = $true
+         }
+         $allRegex = Select-String @splat
+         $matchingStrings = $($allRegex.Matches)
+         foreach ($m in $matchingStrings)
+         {
+            $transcriptText = $($m.Value)
+            $currentRead = get-content $Logfile -Raw
+            $newText = $currentRead.Replace($transcriptText, "")
+            Set-Content -Path $Logfile -Value $newText -Force
+         }
 
-            # Compare the two and put the differences in a third file
-            $m1 = Get-Content -Path $Logfile
-            $m2 = Get-Content -Path $TempFile
-            $all = Compare-Object -ReferenceObject $m1 -DifferenceObject $m2 | Where-Object -Property Sideindicator -eq '<='
-            $Array = [System.Collections.Generic.List[PSObject]]@()
-            foreach ($a in $all)
-            {
-                [void]$Array.Add($($a.InputObject))
-            }
-            $Array = $Array -replace 'VERBOSE: ', ''
+         # Remove 'VERBOSE: '
+         $currentRead = get-content $Logfile -Raw
+         $newText = $currentRead.Replace("VERBOSE: ", "")
+         Set-Content -Path $Logfile -Value $newText -Force
 
-            Remove-Item -Path $Logfile -Force
-            Remove-Item -Path $TempFile -Force
-            # Finally, put the information we care about in the original file and discard the rest.
-            $Array | Out-File $Logfile -Append -Encoding ASCII
-            
-        }
+         # Remove 'Transcript started line'
+         $pattern = "Transcript started, output file is " + $Logfile
+         $currentRead = get-content $Logfile -Raw
+         $newText = $currentRead.Replace($pattern, "")
+         Set-Content -Path $Logfile -Value $newText -Force
+
+         # Remove blank lines
+         $currentRead = Get-Content $Logfile
+         $newText = $currentRead | Where-Object { $_ -notmatch "^$" }
+         Set-Content -Path $Logfile -Value $newText -Force            
+      }
         
-        Start-Log
+      Start-Log
 
-        Function Set-Console
-        {
-            <# 
-        .Synopsis
-        Function to set console colors just for the session.
-        .Description
-        Function to set console colors just for the session.
-        This function sets background to black and foreground to green.
-        Verbose is DarkCyan which is what I use often with logging in scripts.
-        I mainly did this because darkgreen does not look too good on blue (Powershell defaults).
-        .Notes
-        2017-10-19: v1.0 Initial script 
-        #>
+      ####################</Default Begin Block>####################
         
-            Function Test-IsAdmin
-            {
-                <#
-                .Synopsis
-                Determines whether or not the user is a member of the local Administrators security group.
-                .Outputs
-                System.Bool
-                #>
+   }
 
-                [CmdletBinding()]
-    
-                $Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-                $Principal = new-object System.Security.Principal.WindowsPrincipal(${Identity})
-                $IsAdmin = $Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
-                Write-Output -InputObject $IsAdmin
-            }
+   Process
+   {
+      Try
+      {
+         
+         # Your script goes here
+         
+         # Write-Log "hello world"
+         # Write-Log (cmd /c "ipconfig /all") -Color Magenta
+         # Write-Log (get-process | format-table)
 
-            $console = $host.UI.RawUI
-            If (Test-IsAdmin)
-            {
-                $console.WindowTitle = "Administrator: Powershell"
-            }
-            Else
-            {
-                $console.WindowTitle = "Powershell"
-            }
-            $Background = "Black"
-            $Foreground = "Green"
-            $Messages = "DarkCyan"
-            $Host.UI.RawUI.BackgroundColor = $Background
-            $Host.UI.RawUI.ForegroundColor = $Foreground
-            $Host.PrivateData.ErrorForegroundColor = $Messages
-            $Host.PrivateData.ErrorBackgroundColor = $Background
-            $Host.PrivateData.WarningForegroundColor = $Messages
-            $Host.PrivateData.WarningBackgroundColor = $Background
-            $Host.PrivateData.DebugForegroundColor = $Messages
-            $Host.PrivateData.DebugBackgroundColor = $Background
-            $Host.PrivateData.VerboseForegroundColor = $Messages
-            $Host.PrivateData.VerboseBackgroundColor = $Background
-            $Host.PrivateData.ProgressForegroundColor = $Messages
-            $Host.PrivateData.ProgressBackgroundColor = $Background
-            Clear-Host
-        }
-        Set-Console
+      }
+      Catch
+      {
+         Write-Error $($_.Exception.Message)
+      }
+   }
 
-        ####################</Default Begin Block>####################
-
-    }
-    
-    Process
-    {   
-        Try
-        {
-            # Write-Log "hello world"
-            # Write-Log (cmd /c "ipconfig /all") -Color Magenta
-            # Write-Log (get-process | format-table)
-        }
-        Catch
-        {
-            Write-Error $($_.Exception.Message)
-        }
-    }
-
-    End
-    {
-        Stop-log
-    }
+   End
+   {
+      Stop-log
+   }
 }
 
 <#######</Body>#######>
