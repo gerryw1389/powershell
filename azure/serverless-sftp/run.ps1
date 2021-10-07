7989bc9211d4ecf2b8f966fe2b733a58234ac4d8
Gerry Williams <gerryw1389@gmail.com>
2021-10-07 16:40:13 -0500

    move container groups to diff repo

--- azure/serverless-sftp/run.ps1
+++
@@ -1,7 +0,0 @@
-$params = @{
-   "ResourceGroupName"= "Storage-Account-Resource-Group"
-   "TemplateFile"= "t.json"
-   "TemplateParameterFile"= "p.json"
-   "Tag" = @{"Department"="IT"; "Email"="me@company.com";}
-}
-New-AzResourceGroupDeployment @params
